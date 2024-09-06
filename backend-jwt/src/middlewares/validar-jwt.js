76eceb774acefc16ffa66e201eb07e7c26f7284f
Sergiovz <128724116+Sergiovz@users.noreply.github.com>
2024-09-06 16:27:06 -0300

    feat: importando el archivo de conexión

--- backend-jwt/src/middlewares/validar-jwt.js
+++ backend-jwt/src/middlewares/validar-jwt.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 
 import { SECRET_KEY } from "../config/env.js";
-import { database } from "../db/database.js";
+import { Pool } from "../db/database.js";
 
 // Middleware para verificar el token JWT
 export default (req, res, next) => {
