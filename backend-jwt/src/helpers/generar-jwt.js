f4b2b8f762176efd66b58c9d1fc59f3272a55641
SergioVZ <sergiovaldez599@gmail.com>
2024-09-09 00:25:33 -0300

    fix: agregando manejo de errores al generar el jwt

--- backend-jwt/src/helpers/generar-jwt.js
+++ backend-jwt/src/helpers/generar-jwt.js
@@ -3,13 +3,18 @@ import { SECRET_KEY } from "../config/env.js";
 
 const generarJwt = async (userId) => {
   const payload = { userId };
-    const secret = SECRET_KEY;
+  const secret = SECRET_KEY;
 
-  const token = jwt.sign(payload, secret, {
-    expiresIn: "5h",
-  });
+  try {
+    const token = jwt.sign(payload, secret, {
+      expiresIn: "5h",
+    });
 
-  return token;
+    return token;
+  } catch (error) {
+    console.error("Error al generar el token JWT:", error);
+    throw new Error("No se pudo generar el token JWT");
+  }
 };
 
 export default generarJwt;
